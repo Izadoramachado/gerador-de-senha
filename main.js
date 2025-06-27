b5aa29199b2a332ae4a40d01fe73048062d044b5
Izadoramachado <141340726+Izadoramachado@users.noreply.github.com>
2025-06-27 08:01:02 -0300

    hhujrf

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
-numeroSenha.textContent = tamanhoSenha;const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
